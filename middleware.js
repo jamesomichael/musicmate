9c9a8955d64c3025d4ce1702770114d5d49e75c2
James Michael <jomichael1994@gmail.com>
2025-01-14 22:21:06 +0000

    add middleware to check token validity

---
+++ middleware.js
@@ -0,0 +1,54 @@
+import { NextResponse } from 'next/server';
+import axios from 'axios';
+
+const NEXT_PUBLIC_API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+export async function middleware(request) {
+	const { cookies } = request;
+	const accessToken = cookies?.get('access_token')?.value;
+	const refreshToken = cookies?.get('refresh_token')?.value;
+
+	if (!accessToken && !refreshToken) {
+		return NextResponse.redirect(new URL('/login', request.url));
+	}
+
+	if (accessToken) {
+		const response = await axios.get('https://api.spotify.com/v1/me', {
+			headers: {
+				Authorization: `Bearer ${accessToken}`,
+			},
+		});
+
+		if (response.ok) {
+			return NextResponse.next();
+		}
+	}
+
+	if (refreshToken) {
+		const refreshResponse = await axios.post(
+			`${NEXT_PUBLIC_API_URL}/api/refresh-token`,
+			{ refresh_token: refreshToken },
+			{
+				headers: { 'Content-Type': 'application/json' },
+			}
+		);
+
+		if (refreshResponse.status === 200) {
+			const { access_token: newAccessToken, expires_in: expiresIn } =
+				refreshResponse.data;
+
+			const response = NextResponse.next();
+			response.cookies.set('access_token', newAccessToken, {
+				httpOnly: true,
+				maxAge: expiresIn,
+				path: '/',
+			});
+			return response;
+		}
+	}
+	return NextResponse.redirect(new URL('/login', request.url));
+}
+
+export const config = {
+	matcher: ['/'],
+};
