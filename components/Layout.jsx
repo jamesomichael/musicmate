5b651048463c2f05b356b4cb828fb065d366069d
James Michael <jomichael1994@gmail.com>
2025-01-15 18:10:35 +0000

    update navbar; initial home button & search bar styling

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
-
 import { cookies } from 'next/headers';
 
 import Navbar from './Navbar';
 import Player from './Player';
 
+import spotifyService from '@/services/spotify';
+
 const Layout = async ({ children }) => {
 	const cookieStore = await cookies();
 	const accessToken = cookieStore.get('access_token').value;
+	const user = await spotifyService.fetchCurrentUser(accessToken);
 
 	return (
 		<div className="flex flex-col max-h-full h-full">
 			<div className="h-14">
-				<Navbar accessToken={accessToken} />
+				<Navbar accessToken={accessToken} user={user} />
 			</div>
-			<div className="md:p-0 md:m-4 pb-40 flex-1 overflow-hidden">
+			<div className="md:p-0 md:mx-4 pb-40 flex-1 overflow-hidden">
 				{children}
 			</div>
 			<div className="fixed bottom-0 md:relative w-full">
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import Link from 'next/link';
 
-const Navbar = () => {
-	return <div className="h-full bg-green-300">Navbar</div>;
+import { GrHomeRounded } from 'react-icons/gr';
+import { HiMiniMagnifyingGlass } from 'react-icons/hi2';
+
+const Navbar = ({ user }) => {
+	return (
+		<div className="h-full bg-black grid grid-cols-3 items-center">
+			<div>Back</div>
+			<div className="h-full py-2 flex justify-center items-center gap-1">
+				<div className="h-full aspect-square rounded-full bg-neutral-800 flex justify-center items-center text-gray-300 hover:scale-105 hover:cursor-pointer">
+					<GrHomeRounded size={18} />
+				</div>
+				<div className="h-full relative group">
+					<label
+						htmlFor="search"
+						className="absolute inset-y-0 left-0 flex items-center pl-3 text-gray-300 group-hover:text-white group-hover:cursor-pointer"
+					>
+						<HiMiniMagnifyingGlass size={25} />
+					</label>
+					<input
+						id="search"
+						placeholder="What do you want to play?"
+						className="rounded-full bg-neutral-800 h-full w-96 pl-11 font-copy text-sm font-medium placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-white focus:border-transparent group-hover:bg-neutral-700"
+						type="text"
+					/>
+				</div>
+			</div>
+			<div className="flex justify-end items-center">{user.email}</div>
+		</div>
+	);
 };
 
 export default Navbar;
