ba1c0e9759467de90aa700cb57be6b2610d2f298
James Michael <jomichael1994@gmail.com>
2025-01-18 16:38:25 +0000

    update user profile banner, add placeholder image

--- components/UserProfileBanner.jsx
+++ components/UserProfileBanner.jsx
@@ -2,19 +2,32 @@
 import React from 'react';
 
 import useUserStore from '@/stores/userStore';
+// import useLibraryStore from '@/stores/libraryStore';
 
 const UserProfileBanner = () => {
 	const { displayName, followers } = useUserStore();
+	// const { playlists, isLoadingPlaylists } = useLibraryStore();
 
 	return (
-		<div className="grid grid-cols-[auto_1fr] items-end h-full pb-4 px-8">
-			<div className="w-64">Image</div>
+		<div className="grid grid-cols-[auto_1fr] gap-8 items-end h-full pb-4 px-8">
+			<div>
+				<div
+					className="h-56 aspect-square bg-cover bg-center rounded-full"
+					style={{
+						backgroundImage:
+							'url(https://content.imageresizer.com/images/memes/Homer-Napping-meme-4.jpg)',
+					}}
+				></div>
+			</div>
 			<div className="flex flex-col gap-4">
 				<span className="font-copy text-sm">Profile</span>
 				<div className="font-heading text-6xl font-black">
 					{displayName}
 				</div>
-				<div className="font-copy text-sm">
+				<div className="flex gap-4 font-copy text-sm">
+					{/* {!isLoadingPlaylists && (
+						<span>{playlists.length}&nbsp;Playlists</span>
+					)} */}
 					<span>{followers} Followers</span>
 				</div>
 			</div>
