055b85badcb8a10290d35236fea573ec0e8ff721
James Michael <jomichael1994@gmail.com>
2025-01-20 17:25:53 +0000

    cleanup homepage

--- app/(dashboard)/page.js
+++ app/(dashboard)/page.js
@@ -4,8 +4,8 @@ import BrowseCategories from '@/components/BrowseCategories';
 const Home = async () => {
 	return (
 		<div className="text-gray-300 text-6xl font-gloock">
-			<Loader />
-			<BrowseCategories />
+			{/* <Loader /> */}
+			{/* <BrowseCategories /> */}
 			Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
 			maiores ad assumenda veritatis molestiae esse dignissimos, soluta
 			debitis voluptatum dolor! Repudiandae perferendis deserunt et ullam
