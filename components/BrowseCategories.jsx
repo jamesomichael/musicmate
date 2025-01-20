bcc1941dd14816161054e138b86634b5ea83df31
James Michael <jomichael1994@gmail.com>
2025-01-20 17:28:59 +0000

    update categories; support loading state, update structure, add warning message

--- components/BrowseCategories.jsx
+++ components/BrowseCategories.jsx
@@ -1,12 +1,18 @@
 'use client';
 import React, { useEffect } from 'react';
+import Link from 'next/link';
+
+import Loader from './Loader';
 
 import useAuthStore from '@/stores/authStore';
+
 import useCategoryStore from '@/stores/categoryStore';
+import { BiSolidCategory } from 'react-icons/bi';
+import { IoWarning } from 'react-icons/io5';
 
 const BrowseCategories = () => {
 	const { accessToken } = useAuthStore();
-	const { categories, setCategories } = useCategoryStore();
+	const { isLoading, categories, setCategories } = useCategoryStore();
 
 	useEffect(() => {
 		if (accessToken) {
@@ -15,26 +21,53 @@ const BrowseCategories = () => {
 	}, [setCategories, accessToken]);
 
 	return (
-		<div className="grid grid-cols-12">
-			{categories.length > 0 ? (
-				categories.map((category) => {
-					return (
-						<div key={category.id}>
-							<div
-								className="bg-center bg-cover max-w-48 aspect-square flex justify-start items-end p-2"
-								style={{
-									backgroundImage: `url(${category.icons[0].url})`,
-								}}
-							>
-								<span className="font-heading text-base text-white">
-									{category.name}
-								</span>
-							</div>
-						</div>
-					);
-				})
+		<div className="select-none overflow-hidden h-full grid grid-rows-[auto_1fr] gap-4">
+			<div className="flex flex-col gap-2">
+				<div className="flex justify-start items-center gap-2 text-gray-300">
+					<BiSolidCategory size={30} />
+					<span className="font-heading text-base md:text-lg lg:text-xl font-bold">
+						Browse all
+					</span>
+				</div>
+				<div className="flex justify-start items-center">
+					<IoWarning size={22} className="text-amber-400" />
+					<span className="font-copy text-sm text-spotify-green">
+						&nbsp;Unfortunately, this page can no longer function.
+						For more information, please&nbsp;
+						<Link
+							href="https://developer.spotify.com/blog/2024-11-27-changes-to-the-web-api"
+							target="_blank"
+							className="underline hover:text-gray-200"
+						>
+							click here
+						</Link>
+						.
+					</span>
+				</div>
+			</div>
+			{isLoading ? (
+				<Loader />
 			) : (
-				<>Loading...</>
+				<div className="overflow-y-scroll cursor-not-allowed">
+					<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 2xl:grid-cols-7 gap-4">
+						{categories.map((category) => {
+							return (
+								<div
+									key={category.id}
+									className="relative bg-center bg-cover w-full aspect-square flex justify-start items-end p-2 rounded overflow-hidden shadow-md"
+									style={{
+										backgroundImage: `url(${category.icons[0].url})`,
+									}}
+								>
+									<div className="absolute inset-0 bg-gradient-to-t from-black to-transparent opacity-60"></div>
+									<span className="leading-5 relative font-heading text-sm xl:text-base text-gray-200 line-clamp-2">
+										{category.name}
+									</span>
+								</div>
+							);
+						})}
+					</div>
+				</div>
 			)}
 		</div>
 	);
