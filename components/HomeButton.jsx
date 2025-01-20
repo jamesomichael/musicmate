57d0865683a860b56c97be55f46e324c31d10215
James Michael <jomichael1994@gmail.com>
2025-01-20 17:28:08 +0000

    add home button title

--- components/HomeButton.jsx
+++ components/HomeButton.jsx
@@ -11,6 +11,7 @@ const HomeButton = () => {
 	return (
 		<Link
 			href="/"
+			title="Home"
 			className="h-full aspect-square rounded-full bg-neutral-800 flex justify-center items-center text-gray-300 hover:scale-105 hover:cursor-pointer"
 		>
 			{isHomepage ? <GoHomeFill size={22} /> : <GoHome size={22} />}
