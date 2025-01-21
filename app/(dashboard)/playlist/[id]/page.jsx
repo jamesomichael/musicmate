0272ccf796f254bc0cdf66707d0573e7582aa5fe
James Michael <jomichael1994@gmail.com>
2025-01-21 22:30:39 +0000

    update playlist page; fetch data and display using components

--- app/(dashboard)/playlist/[id]/page.jsx
+++ app/(dashboard)/playlist/[id]/page.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
+import { cookies } from 'next/headers';
+
+import spotifyService from '@/services/spotify';
+
+import PlaylistDetailed from '@/components/PlaylistDetailed';
 
 const Playlist = async ({ params }) => {
 	const { id } = await params;
-	return <div>Playlist {id}</div>;
+	const cookieStore = await cookies();
+	const accessToken = cookieStore.get('access_token')?.value;
+	const { tracks, ...metadata } = await spotifyService.fetchPlaylistById(
+		id,
+		accessToken
+	);
+	const playlist = {
+		metadata: {
+			...metadata,
+			imageUrl: metadata.images[0].url,
+			size: tracks.items.length,
+		},
+		tracks: tracks.items,
+	};
+	console.log('playlist', playlist);
+	return (
+		<div>
+			<PlaylistDetailed playlist={playlist} />
+		</div>
+	);
 };
 
 export default Playlist;
