d2387f209d28e3e803332184dd7bfd77e4ae239e
James Michael <jomichael1994@gmail.com>
2025-01-29 01:15:55 +0000

    add wip artist header

--- app/(dashboard)/artist/[id]/page.jsx
+++ app/(dashboard)/artist/[id]/page.jsx
@@ -2,6 +2,9 @@
 import { useParams } from 'next/navigation';
 import React from 'react';
 
+import Loader from '@/components/Loader';
+import ArtistHeader from '@/components/ArtistHeader';
+
 import useArtist from '@/hooks/useArtist';
 
 import useAuthStore from '@/stores/authStore';
@@ -10,12 +13,29 @@ const Artist = () => {
 	const { id } = useParams();
 	const { accessToken } = useAuthStore();
 
-	const { artist, artistAlbums, topTracks } = useArtist(id, accessToken);
+	const { artist, artistAlbums, topTracks, isLoadingArtist } = useArtist(
+		id,
+		accessToken
+	);
 
 	console.error('artist', artist);
-	console.error('artistAlbums', artistAlbums);
-	console.error('topTracks', topTracks);
-	return <div>Artist {id}</div>;
+	// console.error('artistAlbums', artistAlbums);
+	// console.error('topTracks', topTracks);
+	return (
+		<div className="h-full">
+			<div className="select-none grid grid-rows-[auto,1fr] h-full">
+				<div className="h-80">
+					{isLoadingArtist ? (
+						<Loader />
+					) : (
+						<>
+							<ArtistHeader artist={artist} />
+						</>
+					)}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Artist;
---
+++ components/ArtistHeader.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+
+import Loader from './Loader';
+
+import useDynamicGradient from '@/hooks/useDynamicGradient';
+
+const ArtistHeader = ({ artist }) => {
+	const { gradient, isLoading } = useDynamicGradient(artist.images[0].url);
+	return isLoading ? (
+		<div className="h-full">
+			<Loader />
+		</div>
+	) : (
+		<div
+			className="h-full p-6 grid grid-cols-[auto_1fr] gap-6"
+			style={{ background: gradient }}
+		>
+			<div
+				className="shadow-xl h-full aspect-square bg-cover bg-center rounded-full"
+				style={{ backgroundImage: `url(${artist.images[0].url})` }}
+			></div>
+			<div className="flex flex-col justify-end gap-4">
+				<span className="font-copy text-xs">Artist</span>
+				<span className="font-heading font-black text-6xl">
+					{artist.name}
+				</span>
+				<div className="flex items-center gap-2">
+					<span className="text-sm font-copy">
+						{artist.followers.total.toLocaleString()} followers
+					</span>
+					{artist.genres.length > 0 && (
+						<div className="text-xs font-copy gap-2 flex items-center">
+							<span className="text-gray-300">•</span>
+							{artist.genres.map((genre) => {
+								return (
+									<span
+										key={genre}
+										className="bg-gradient-to-r from-neutral-800 to-black text-spotify-green px-3 py-1 rounded-full font-bold"
+									>
+										{genre}
+									</span>
+								);
+							})}
+						</div>
+					)}
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default ArtistHeader;
