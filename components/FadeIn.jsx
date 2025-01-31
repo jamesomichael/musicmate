6bb32bd2b8d1b28e4e2ba776efb285330b1ec7e3
James Michael <jomichael1994@gmail.com>
2025-01-31 13:57:56 +0000

    add link support to FadeIn and FadeInSlide components

--- components/FadeIn.jsx
+++ components/FadeIn.jsx
@@ -1,10 +1,12 @@
 'use client';
 import React from 'react';
+import Link from 'next/link';
 import { motion } from 'framer-motion';
 
 const FadeIn = ({
 	children,
 	className,
+	href,
 	delay = 0,
 	duration = 0.5,
 	x = 50,
@@ -19,7 +21,13 @@ const FadeIn = ({
 			transition={{ duration, delay }}
 			{...props}
 		>
-			{children}
+			{href ? (
+				<Link className="hover:underline" href={href}>
+					{children}
+				</Link>
+			) : (
+				children
+			)}
 		</motion.div>
 	);
 };
--- components/FadeInSlide.jsx
+++ components/FadeInSlide.jsx
@@ -1,10 +1,12 @@
 'use client';
 import React from 'react';
+import Link from 'next/link';
 import { motion } from 'framer-motion';
 
 const FadeInSlide = ({
 	children,
 	className,
+	href,
 	delay = 0,
 	duration = 0.5,
 	x = 50,
@@ -19,7 +21,13 @@ const FadeInSlide = ({
 			transition={{ duration, delay }}
 			{...props}
 		>
-			{children}
+			{href ? (
+				<Link className="hover:underline" href={href}>
+					{children}
+				</Link>
+			) : (
+				children
+			)}
 		</motion.div>
 	);
 };
