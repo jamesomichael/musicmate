1c2388bf17b484500e1b33238503b6c92e8803fe
James Michael <jomichael1994@gmail.com>
2025-02-04 21:37:46 +0000

    add placeholder div

--- app/(dashboard)/page.js
+++ app/(dashboard)/page.js
@@ -3,9 +3,12 @@ import BrowseCategories from '@/components/BrowseCategories';
 
 const Home = async () => {
 	return (
-		<div className="text-gray-300 text-6xl font-gloock p-8">
+		<div className="flex flex-col gap-6 text-gray-300 text-6xl font-gloock p-8">
 			{/* <Loader /> */}
 			{/* <BrowseCategories /> */}
+			<div className="h-72 bg-neutral-700 rounded">
+				<Loader />
+			</div>
 			Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
 			maiores ad assumenda veritatis molestiae esse dignissimos, soluta
 			debitis voluptatum dolor! Repudiandae perferendis deserunt et ullam
