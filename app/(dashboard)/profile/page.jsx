2e6e7e9e66b86ed718b84039d6983b36d3f04a68
James Michael <jomichael1994@gmail.com>
2025-04-07 22:10:49 +0100

    show recently played albums on user profile page

--- app/(dashboard)/profile/page.jsx
+++ app/(dashboard)/profile/page.jsx
@@ -1,18 +1,35 @@
 'use client';
-import React from 'react';
+import React, { useMemo } from 'react';
 
 import UserProfileBanner from '@/components/UserProfileBanner';
 import DiscographyCarousel from '@/components/DiscographyCarousel';
 import TopTracks from '@/components/TopTracks';
 import Loader from '@/components/Loader';
 
-import useTopItems from '@/hooks/useTopItems';
+import useUserProfile from '@/hooks/useUserProfile';
 
 import useAuthStore from '@/stores/authStore';
 
 const Profile = () => {
 	const { accessToken } = useAuthStore();
-	const { isLoading, topTracks, topArtists } = useTopItems(accessToken);
+	const {
+		isLoading,
+		topTracks,
+		topArtists,
+		isLoadingRecentlyPlayed,
+		recentlyPlayed,
+	} = useUserProfile(accessToken);
+
+	const recentlyPlayedAlbums = useMemo(() => {
+		return [
+			...new Map(
+				recentlyPlayed
+					?.map((item) => item.track?.album)
+					.filter(Boolean)
+					.map((album) => [album.id, album])
+			).values(),
+		];
+	}, [recentlyPlayed]);
 
 	return (
 		<div className="flex flex-col">
@@ -47,6 +64,19 @@ const Profile = () => {
 							</span>
 							<TopTracks data={topTracks} showArtists={true} />
 						</div>
+						{isLoadingRecentlyPlayed ? (
+							<Loader />
+						) : (
+							<div className="flex flex-col gap-2">
+								<span className="font-heading font-bold text-xl leading-none">
+									Recently played albums
+								</span>
+								<DiscographyCarousel
+									data={recentlyPlayedAlbums}
+									type="album"
+								/>
+							</div>
+						)}
 					</>
 				)}
 			</div>
