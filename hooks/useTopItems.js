33d4923e95bc8e26058515444193953ebf0a1381
James Michael <jomichael1994@gmail.com>
2025-04-07 21:32:35 +0100

    add top items hook

---
+++ hooks/useTopItems.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react';
+import spotifyService from '@/services/spotify';
+
+const useTopItems = (accessToken) => {
+	const [topTracks, setTopTracks] = useState([]);
+	const [topArtists, setTopArtists] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+
+	useEffect(() => {
+		const fetchTopItems = async () => {
+			setIsLoading(true);
+			const { items: tracks } = await spotifyService.fetchUserTopTracks(
+				accessToken
+			);
+			const { items: artists } = await spotifyService.fetchUserTopArtists(
+				accessToken
+			);
+
+			setTopTracks(tracks);
+			setTopArtists(artists);
+			setIsLoading(false);
+		};
+
+		if (accessToken) {
+			fetchTopItems();
+		}
+	}, [accessToken]);
+
+	return { isLoading, topTracks, topArtists };
+};
+
+export default useTopItems;
