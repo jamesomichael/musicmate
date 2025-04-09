be06810737cbd38160a65195a9e7274518ea4bb5
James Michael <jomichael1994@gmail.com>
2025-04-09 21:23:08 +0100

    wip homepage

--- app/(dashboard)/page.js
+++ app/(dashboard)/page.js
@@ -1,60 +1,18 @@
 import Loader from '@/components/Loader';
 import BrowseCategories from '@/components/BrowseCategories';
+import UserTopItems from '@/components/UserTopItems';
 
 const Home = async () => {
 	return (
-		<div className="flex flex-col gap-6 text-gray-300 text-6xl font-gloock p-8">
+		<div className="flex flex-col gap-6 p-6">
 			{/* <Loader /> */}
 			{/* <BrowseCategories /> */}
 			<div className="h-72 bg-neutral-700 rounded">
 				<Loader />
 			</div>
-			Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
-			maiores ad assumenda veritatis molestiae esse dignissimos, soluta
-			debitis voluptatum dolor! Repudiandae perferendis deserunt et ullam
-			quaerat architecto facere hic? Eum. Lorem ipsum dolor sit amet
-			consectetur adipisicing elit. Odit maiores ad assumenda veritatis
-			molestiae esse dignissimos, soluta debitis voluptatum dolor!
-			Repudiandae perferendis deserunt et ullam quaerat architecto facere
-			hic? Eum. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-			Odit maiores ad assumenda veritatis molestiae esse dignissimos,
-			soluta debitis voluptatum dolor! Repudiandae perferendis deserunt et
-			ullam quaerat architecto facere hic? Eum. Lorem ipsum dolor sit amet
-			consectetur adipisicing elit. Odit maiores ad assumenda veritatis
-			molestiae esse dignissimos, soluta debitis voluptatum dolor!
-			Repudiandae perferendis deserunt et ullam quaerat architecto facere
-			hic? Eum. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-			Odit maiores ad assumenda veritatis molestiae esse dignissimos,
-			soluta debitis voluptatum dolor! Repudiandae perferendis deserunt et
-			ullam quaerat architecto facere hic? Eum. Lorem ipsum dolor sit amet
-			consectetur adipisicing elit. Odit maiores ad assumenda veritatis
-			molestiae esse dignissimos, soluta debitis voluptatum dolor!
-			Repudiandae perferendis deserunt et ullam quaerat architecto facere
-			hic? Eum. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-			Odit maiores ad assumenda veritatis molestiae esse dignissimos,
-			soluta debitis voluptatum dolor! Repudiandae perferendis deserunt et
-			ullam quaerat architecto facere hic? Eum. Lorem ipsum dolor sit amet
-			consectetur adipisicing elit. Odit maiores ad assumenda veritatis
-			molestiae esse dignissimos, soluta debitis voluptatum dolor!
-			Repudiandae perferendis deserunt et ullam quaerat architecto facere
-			hic? Eum. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-			Odit maiores ad assumenda veritatis molestiae esse dignissimos,
-			soluta debitis voluptatum dolor! Repudiandae perferendis deserunt et
-			ullam quaerat architecto facere hic? Eum. Lorem ipsum dolor sit amet
-			consectetur adipisicing elit. Odit maiores ad assumenda veritatis
-			molestiae esse dignissimos, soluta debitis voluptatum dolor!
-			Repudiandae perferendis deserunt et ullam quaerat architecto facere
-			hic? Eum. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-			Odit maiores ad assumenda veritatis molestiae esse dignissimos,
-			soluta debitis voluptatum dolor! Repudiandae perferendis deserunt et
-			ullam quaerat architecto facere hic? Eum. Lorem ipsum dolor sit amet
-			consectetur adipisicing elit. Odit maiores ad assumenda veritatis
-			molestiae esse dignissimos, soluta debitis voluptatum dolor!
-			Repudiandae perferendis deserunt et ullam quaerat architecto facere
-			hic? Eum. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-			Odit maiores ad assumenda veritatis molestiae esse dignissimos,
-			soluta debitis voluptatum dolor! Repudiandae perferendis deserunt et
-			ullam quaerat architecto facere hic? Eum.
+			<div className="">
+				<UserTopItems />
+			</div>
 		</div>
 	);
 };
