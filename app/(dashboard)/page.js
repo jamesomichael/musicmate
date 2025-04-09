8b95d04e79a0a04c4ad7b49639185beb53f1358e
James Michael <jomichael1994@gmail.com>
2025-04-09 23:05:30 +0100

    show recently played albums on homepage

--- app/(dashboard)/page.js
+++ app/(dashboard)/page.js
@@ -1,13 +1,16 @@
+'use client';
 import UserTopItems from '@/components/UserTopItems';
 import SuggestionsGrid from '@/components/SuggestionsGrid';
+import RecentlyPlayed from '@/components/RecentlyPlayed';
 
-const Home = async () => {
+const Home = () => {
 	return (
 		<div className="flex flex-col">
 			<div className="h-fit bg-gradient-to-b from-neutral-700 to-spotify-black">
 				<SuggestionsGrid />
 			</div>
 			<div className="flex flex-col gap-6 p-6">
+				<RecentlyPlayed />
 				<UserTopItems />
 			</div>
 		</div>
