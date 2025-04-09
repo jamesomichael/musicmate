8c8cb8fcfd872a810a380ed2eb7ff7ae21989c9f
James Michael <jomichael1994@gmail.com>
2025-04-09 23:01:15 +0100

    use playlists to show initial homepage suggestions

--- app/(dashboard)/page.js
+++ app/(dashboard)/page.js
@@ -1,16 +1,13 @@
-import Loader from '@/components/Loader';
-import BrowseCategories from '@/components/BrowseCategories';
 import UserTopItems from '@/components/UserTopItems';
+import SuggestionsGrid from '@/components/SuggestionsGrid';
 
 const Home = async () => {
 	return (
-		<div className="flex flex-col gap-6 p-6">
-			{/* <Loader /> */}
-			{/* <BrowseCategories /> */}
-			<div className="h-72 bg-neutral-700 rounded">
-				<Loader />
+		<div className="flex flex-col">
+			<div className="h-fit bg-gradient-to-b from-neutral-700 to-spotify-black">
+				<SuggestionsGrid />
 			</div>
-			<div className="">
+			<div className="flex flex-col gap-6 p-6">
 				<UserTopItems />
 			</div>
 		</div>
---
+++ components/SuggestionsGrid.jsx
@@ -0,0 +1,45 @@
+'use client';
+import React from 'react';
+import Link from 'next/link';
+
+import Loader from './Loader';
+
+import useLibraryStore from '@/stores/libraryStore';
+
+const SuggestionsGrid = () => {
+	const { isLoadingPlaylists, playlists } = useLibraryStore();
+
+	return isLoadingPlaylists ? (
+		<div className="h-80">
+			<Loader />
+		</div>
+	) : (
+		<div className="p-6 grid gap-3 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+			{playlists.slice(1, 9).map((playlist, i) => {
+				return (
+					<Link
+						key={playlist.id}
+						href={`/playlist/${playlist.id}`}
+						className="transition-all duration-200 grid grid-cols-[auto,1fr] gap-2 rounded h-[4.5rem] bg-white bg-opacity-5 hover:bg-opacity-15"
+					>
+						<div
+							className="h-full aspect-square bg-cover bg-center rounded-l"
+							style={
+								playlist.images?.length > 0
+									? {
+											backgroundImage: `url(${playlist.images[0].url})`,
+									  }
+									: {}
+							}
+						></div>
+						<span className="p-2 flex items-center font-medium font-heading text-sm line-clamp-2">
+							{playlist.name}
+						</span>
+					</Link>
+				);
+			})}
+		</div>
+	);
+};
+
+export default SuggestionsGrid;
